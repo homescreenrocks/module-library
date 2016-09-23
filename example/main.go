ef6de4b4448066227718a9cb57ca8c57fa11713b
Sven Walter <sven@wltr.eu>
2016-09-23 19:46:08 +0200

    adding settings to module
    also:
    * s/plugin/module/
    * changed module API

--- example/main.go
+++ example/main.go
@@ -5,33 +5,35 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/homescreenrocks/plugin-library"
+	"github.com/homescreenrocks/homescreen/shared"
+	"github.com/homescreenrocks/module-library"
 )
 
 func main() {
-	p := &plugin.Plugin{
-		Metadata: plugin.Metadata{
-			ID:          "example-plugin",
-			Name:        "Example Plugin",
-			Version:     "v0.0",
-			Description: "An example plugin.",
-		},
-		Settings: map[string]plugin.Setting{
-			"tz": plugin.Setting{
-				Default:   "",
-				Type:      "string",
-				Mandatory: false,
-				Value:     "Berlin",
-			},
-		},
-		RouteSetup: func(group *gin.RouterGroup) error {
-			group.GET("/date", func(c *gin.Context) {
-				c.JSON(http.StatusOK, time.Now().String())
-			})
+	m := new(module.Module)
 
-			return nil
-		},
+	m.Metadata = &shared.ModuleMetadata{
+		ID:          "example-module",
+		Name:        "Example Module",
+		Version:     "v0.0",
+		Description: "An example module.",
 	}
 
-	p.Main()
+	m.Settings = &shared.ModuleSettings{
+		shared.ModuleSetting{
+			Name:      "timezone",
+			Default:   "Berlin",
+			Type:      "string",
+			Mandatory: false,
+		},
+	}
+	m.RouteSetup = func(group *gin.RouterGroup) error {
+		group.GET("/date", func(c *gin.Context) {
+			c.JSON(http.StatusOK, time.Now().String())
+		})
+
+		return nil
+	}
+
+	m.Main()
 }
